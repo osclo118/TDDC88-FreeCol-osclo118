af14979aaa17ead2e2b6deba141b38803dc477df
Carl Jönsson <carjo905@student.liu.se>
2018-10-08 17:39:32 +0200

    Added comment to SoundTest.java

--- test/src/net/sf/freecol/common/sound/SoundTest.java
+++ test/src/net/sf/freecol/common/sound/SoundTest.java
@@ -63,7 +63,7 @@ public class SoundTest extends FreeColTestCase {
         soundPlayer = null;
     }
 
-    private void playSound(String id) {
+    private void playSound(String id) {  // This is just a test comment.
         File file = ResourceManager.getAudio(id);
         assertNotNull("No sound resource: " + id, file);
         try {
